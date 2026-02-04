b61160d1e09c66b745cac04f860479cfd5f4d9fe
Ben Eng <whysguy52@gmail.com>
2026-02-04 00:20:42 -0600

    Update network_manager.gd

--- network_handler/network_manager.gd
+++ network_handler/network_manager.gd
@@ -4,3 +4,21 @@ const IP_ADDRESS: String = "localhost"
 const PORT: int = 1337
 
 var peer: ENetMultiplayerPeer
+
+func _ready():
+	peer = ENetMultiplayerPeer.new()
+
+func host():
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+	
+	multiplayer.peer_connected.connect(peer_connecting)
+
+func join():
+	peer.create_client(IP_ADDRESS, PORT)
+	multiplayer.multiplayer_peer = peer
+
+func peer_connecting(pid):
+	print("Peer " + str(pid) + " has joined the game!")
+	#add_player(pid)
+	
